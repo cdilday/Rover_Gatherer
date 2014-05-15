faa9c22ab47a3289a5178c6f1a45605d8f85b7fe
khhle <khhle@ucsc.edu>
2014-05-14 22:37:22 -0700

    back to menu button added in tempo level
    still conflict with background music

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class BackToMenu : MonoBehaviour {
+	private Color mouseOverColor = Color.cyan;
+	private Color originalColor ;
+	
+	
+	void OnMouseEnter()
+	{
+		originalColor = gameObject.renderer.material.GetColor ("_Color");
+		gameObject.renderer.material.color = mouseOverColor;
+		
+	}
+	
+	void OnMouseExit()
+	{
+		gameObject.renderer.material.color = originalColor;
+		
+	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 	void OnMouseDown()
 	{
 		GameObject.FindGameObjectWithTag ("Sound Player").GetComponent<SoundPlayerScript> ().switchToMenuMusic ();
