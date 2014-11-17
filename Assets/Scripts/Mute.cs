3e3523c7b2dc66a6b9515f22ec1cc18ff18de9ed
cdilday <seesevn@cox.net>
2014-11-17 14:12:40 -0800

    Mute Button.
    Hey. I added a Mute Button from the third project last year (thanks
    Khan) and fixed some of the code when I uploaded it today.

---
+++ Assets/Scripts/Mute.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mute : MonoBehaviour {
+	public GUIStyle mute;
+	public GUIStyle unmute;
+
+	public float savedVolume;
+	
+	// Use this for initialization
+	void Start () {
+		savedVolume = AudioListener.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		if (AudioListener.volume != 0 && GUI.Button (new Rect(950,575,50,50), "", mute)) {
+			savedVolume = AudioListener.volume;
+			AudioListener.volume = 0;
+		}
+		else if (AudioListener.volume == 0 && GUI.Button (new Rect(950,575,50,50), "", unmute)) {
+			AudioListener.volume = savedVolume;
+		}
+	}
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+}
---
+++ Assets/Scripts/starScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starScript : MonoBehaviour {
+	
+	public int starType;
+	// Use this for initialization
+	void Start () {
+		if (this.transform.parent.GetComponent<scorePanel> ().starsEarned < starType) {
+			this.transform.position = new Vector2 (50, 50);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
